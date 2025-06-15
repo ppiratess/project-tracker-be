32966e3e1aed86b5d77176df1a70e43d00feca9a
Shubham <tamrakarshubham23@gmail.com>
2025-06-15 08:38:18 +0000

    chore: update project members entity

--- src/database/core/project-members.entity.ts
+++ src/database/core/project-members.entity.ts
@@ -1,6 +1,11 @@
-import { Column, Entity } from 'typeorm';
-import { BaseEntity } from './base.entity';
+import { Column, Entity, ManyToOne, JoinColumn, Index } from 'typeorm';
 
+import { User } from './user.entity';
+import { BaseEntity } from './base.entity';
+import { Project } from './project.entity';
+import { UserRole } from 'src/enums/user-role.enums';
+
+@Index(['projectId', 'userId'], { unique: true })
 @Entity()
 export class ProjectMembers extends BaseEntity {
   @Column()
@@ -9,9 +14,23 @@ export class ProjectMembers extends BaseEntity {
   @Column()
   userId: string;
 
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+  })
+  role: UserRole;
+
   @Column({ default: true })
   isActive: boolean;
 
   @Column({ default: false })
   isDeleted: boolean;
+
+  @ManyToOne(() => Project, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'projectId' })
+  project: Project;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
 }
