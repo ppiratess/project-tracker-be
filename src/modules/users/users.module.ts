71daa35d8b62f1bd7ee87cc3f14fd443228eefce
Shubham <tamrakarshubham23@gmail.com>
2025-08-02 09:52:47 +0000

    feat: enable avatar upload during user registration and integrate Cloudinary service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from 'src/database/core/user.entity';
 import { UsersController } from './users.controller';
+import { CloudinaryModule } from 'src/config/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), CloudinaryModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
